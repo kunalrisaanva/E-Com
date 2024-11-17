936d83d8582536f0b2dd0564f1c9576ecb9c274a
kunalrisaanva <kunalrisaanva12@gmail.com>
2024-11-17 12:29:34 +0530

    add header and footer components

--- fronted/src/app/page.js
+++ fronted/src/app/page.js
@@ -1,20 +1,17 @@
+import Header from "../components/Header";
 import Footer from "@/components/Footer";
 
-import Header from "./components/Header";
 // import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div >
 
+    <Header/>
     <Footer/>
 
 
-<<<<<<< HEAD
-=======
       {/* <h1 className="text-white text-5xl"> Kunal </h1> */}
-    <Header/>
->>>>>>> new-ecomerce
     </div>
   );
 }
