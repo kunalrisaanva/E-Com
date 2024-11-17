bcfc09a8e20eb126d140feab3d27cda5d7ec27d6
kunalrisaanva <kunalrisaanva12@gmail.com>
2024-11-17 10:55:16 +0530

    fix folder structure

--- fronted/src/app/page.js
+++ fronted/src/app/page.js
@@ -1,9 +1,13 @@
 
+import Header from "./components/Header";
+// import Footer from "./components/Footer";
+
 export default function Home() {
   return (
     <div className="">
 
-      <h1 className="text-white text-5xl"> Kunal </h1>
+      {/* <h1 className="text-white text-5xl"> Kunal </h1> */}
+    <Header/>
     </div>
   );
 }
