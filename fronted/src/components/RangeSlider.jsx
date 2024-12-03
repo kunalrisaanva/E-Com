9f333a44d94c60ab871c34c8c8fc08d67896e9df
kunalrisaanva <kunalrisaanva12@gmail.com>
2024-12-03 11:40:22 +0530

    updated code

---
+++ fronted/src/components/RangeSlider.jsx
@@ -0,0 +1,9 @@
+{/* This component can be found in views/admin/dashboards/carInterface */}
+{/* There is a fully customized example */}
+export default function Example() {
+    return (
+    <div className="flex gap-10">
+      <RangeSlider/>
+    </div>
+    );
+  }
