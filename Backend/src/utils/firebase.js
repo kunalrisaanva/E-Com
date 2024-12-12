199642657db6c38120eb4623ba1563beecc0db83
kunalrisaanva <kunalrisaanva12@gmail.com>
2024-12-12 14:37:29 +0530

    change variable and delete image after update

--- Backend/src/utils/firebase.js
+++ Backend/src/utils/firebase.js
@@ -1,10 +1,10 @@
 import admin from "firebase-admin";
 import serviceAccount from "../../fir-ef23f-firebase-adminsdk-3u0vg-d46030c41e.json" with { type: "json" };
-
+import fs from "fs"
 // Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "fir-ef23f.appspot.com", // Replace with your bucket name
+  storageBucket: process.env.BUCKET_NAME, 
 });
 
 const bucket = admin.storage().bucket();
@@ -13,16 +13,19 @@ export const uploadFile = async (localFilePath, destination) => {
   try {
     // Upload the file to Firebase Storage
     const [file] = await bucket.upload(localFilePath, {
-      destination, // Specify the destination path in the bucket
+      destination, 
       metadata: {
-        cacheControl: "public, max-age=31536000", // Cache settings
+        cacheControl: "public, max-age=31536000", 
       },
     });
 
-    console.log(`File uploaded to ${destination}`);
+    // console.log(`File uploaded to ${destination}`);
 
     // Make the file publicly accessible
     await file.makePublic();
+
+    // after upload into firebase delete image from local 
+    fs.unlinkSync(localFilePath) 
 
     // Construct the public URL
     const publicUrl = `https://storage.googleapis.com/${bucket.name}/${destination}`;
