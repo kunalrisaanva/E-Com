9ae3451e1e43ca455085df98d4d9826fa09b1af4
kunalrisaanva <kunalrisaanva12@gmail.com>
2025-02-17 16:52:01 +0530

    update development colors

--- fronted/src/components/Development.jsx
+++ fronted/src/components/Development.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Development = () => {
   return (
-    <div className='fixed bottom-0 w-full left-0 bg-gray-900 text-white flex justify-around py-3 '>
+    <div className='fixed bottom-0 w-full left-0 bg-yellow-300 text-red-900 flex justify-around py-3 '>
 
 
     This website is under development. Please visit later.
