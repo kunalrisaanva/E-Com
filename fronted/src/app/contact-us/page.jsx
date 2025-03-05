6fdb93069773977576ed698c48c057d852dd5619
kunalrisaanva <kunalrisaanva12@gmail.com>
2025-03-05 22:43:27 +0530

    refactor: clean up contact us page by removing commented code and updating import paths

--- fronted/src/app/contact-us/page.jsx
+++ fronted/src/app/contact-us/page.jsx
@@ -1,123 +1,12 @@
-// "use client";
-// import React from "react";
-// import Link from "next/link";
-// import Image from "next/image";
-// import Call from "../../../public/images/call 1.png";
-// import Navbar from "@/components/Navbar";
-// import Header from "@/components/Header";
-// import Footer from "@/components/Footer";
-
-// // import Call from "../../../public/images/Call.jpg"
-
-// const page = () => {
-//   return (
-//     <>
-//       <div className="flex-col ">
-        
-//         <div className="flex absolute left-[6.8%] right-[7.33%] top-[24.2%] bottom-[11.1%] mt-3 bg-[#FFFFFF] shadow-[0px 2px 4px ]">
-//           <div className=" flex bg-[#40BFFF] w-[50%] h-[100%]">
-//             <Image
-//               src={Call}
-//               alt="call"
-//               width={50}
-//               height={46}
-//               className="pt-0 mt-[-14px] ml-3 w-[50%] h-[100%]"
-//             />
-//             <div>
-//               <div className="absolute left-[27.34%] right-[60.25%] top-[7.15%] bottom-[71.32%] w-[156.2px] h-[137.56px]">
-//                 <h1 className="font-[Raleway] font-[400]  leading-[47px] text-5xl lowercase text-[#FFFFFF] ">
-//                   {" "}
-//                   Get in touch
-//                 </h1>
-//               </div>
-//               <div className="flex-col absolute left-[ 29.44%] right-[ 61.96%] top-[38.99%] bottom-[65.9%] flex-wrap ">
-//                 <h1 className="font-[Raleway] font-[400]  leading-[16px] text-xl  lowercase text-[#FFFFFF] items-end mt-7">
-//                   contact@e-comm.ng
-//                 </h1>
-//                 <h1 className="font-[Raleway] font-[400]  leading-[16px] text-xl  lowercase text-[#FFFFFF] items-end mt-8 ml-2 ">
-//                   +234 4556 6665 34{" "}
-//                 </h1>
-//                 <h1 className="font-[Raleway] font-[400]  leading-[16px] text-xl  lowercase text-[#FFFFFF] items-end mt-8 ml-2 ">
-//                   20 Prince Hakerem Lekki Phase 1, Lagos.
-//                 </h1>
-//               </div>
-//             </div>
-//             <div></div>
-//           </div>
-//           <div></div>
-//         </div>
-//         <div className="items-end pt-[54.5rem] px-[27.75rem] ">
-//           <form
-//             className="flex items-center"
-//             onChange={() => console.log("runnig")}
-//           >
-
-// <div className="flex pt-[110px]   items-center justify-center">
-//         <div className="flex w-[635px] ">
-//           <input
-//             className="rounded-l-md h-[64px] w-full pl-4 pr-[80px] rounded-sm border-[2px] border-[#40BFFF]"
-//             type="text"
-//             placeholder="Search query"
-//           />
-
-//           <button className="rounded-r-md w-[127px] text-white bg-[#40BFFF] font-semibold text-[20px]">
-//             Search
-//           </button>
-//         </div>
-//       </div>
-//             {/* <input
-//               type="text"
-//               value={""}
-//               // onChange={(e) => setQuery(e.target.value)}
-//               placeholder="Search query..."
-//               className="w-full px-4 py-2 border border-blue-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 h-20"
-//             />
-//             <button
-//               type="submit"
-//               className="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-600 h-20 text-2xl"
-//             >
-//               Search
-//             </button> */}
-//           </form>
-//         </div>
-//       </div>
-     
-//     </>
-//   );
-// };
-
-// export default page;
-// /* +234 4556 6665 34 */
-
-// // position: absolute;
-// // left: 29.34%;
-// // right: 59.77%;
-// // top: 35.21%;
-// // bottom: 63.68%;
-
-// // font-family: 'Raleway';
-// // font-style: normal;
-// // font-weight: 500;
-// // font-size: 14px;
-// // line-height: 16px;
-// // display: flex;
-// // align-items: flex-end;
-// // text-transform: capitalize;
-
-// // co
-// // 20 Prince Hakerem Lekki
-// // Phase 1, Lagos.
-
-
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/Input.jsx";
+import { Button } from "@/components/ui/button.jsx";
+import { Textarea } from "@/components/ui/textarea.jsx";
 import { toast } from "sonner";
 import CotactUsImage from "../../../public/images/call 1.png";
-import UpperLine from "@/components/UpperLine";
+import UpperLine from "@/components/UpperLine.jsx";
 
 const ContactUs = () => {
   const handleSubmit = (e) => {
