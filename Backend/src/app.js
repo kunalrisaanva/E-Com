8c0573abf239e048e5bf1786fd7854756bece827
kunalrisaanva <kunalrisaanva12@gmail.com>
2025-03-05 22:47:39 +0530

    fix: update CORS origin to use environment variable for better configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -27,7 +27,7 @@ fastify.register(fastifyMultipart, {
 //cors implimentation
 
 fastify.register(fastifyCors, {
-  origin: "http://localhost:3001", 
+  origin: process.env.FRONTEND_URL, 
   methods: ["GET", "POST", "PUT", "DELETE"], 
   allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true, 
